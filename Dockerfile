d65a67e499e10cca30b631944b26675b2e877634
Vasques <76536544+NunoVz@users.noreply.github.com>
2026-06-05 15:04:42 +0100

    Enhancement to cicd

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,12 @@ RUN go build -o scalabit-challenge-api ./api
 FROM alpine:latest
 WORKDIR /app
 
+RUN addgroup -S appgroup && adduser -S nonroot -G appgroup
+
 COPY --from=builder /app/scalabit-challenge-api .
 COPY --from=builder /app/static ./static
+
+USER nonroot
 
 EXPOSE 8080
 CMD ["./scalabit-challenge-api"]
--- api/main.go
+++ api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/NunoVz/Scalabit-Challenge/internal/github"
@@ -52,7 +55,22 @@ func main() {
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("Error: %v", err)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
 	}
+	log.Println("Server exiting cleanly")
 }
